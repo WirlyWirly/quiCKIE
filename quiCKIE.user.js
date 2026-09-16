ad06297965f459b3bf3f2aa82315a346fc3fcc41
verinikat <github.debunk664@passmail.com>
2026-09-16 09:20:08 +0800

    add malayabits

--- quiCKIE.user.js
+++ quiCKIE.user.js
@@ -176,6 +176,11 @@
 // @match   https://luminarr.me/playlists/*
 // @match   https://luminarr.me/torrents*
 
+// @match   https://malayabits.cc
+// @match   https://malayabits.cc/*/bookmarks
+// @match   https://malayabits.cc/playlists/*
+// @match   https://malayabits.cc/torrents*
+
 // @match   https://materialize.is/collages.php?id=*
 // @match   https://materialize.is/top10.php*
 // @match   https://materialize.is/torrents.php*
@@ -518,6 +523,12 @@ const settingsPanelTrackers = [
         trackerName: 'Luminarr', // @holy-elbow
         homepageURL: 'https://lumniarr.me',
         primaryDomain: 'luminarr',
+    },
+
+    {
+        trackerName: 'MalayaBits', // @holy-elbow
+        homepageURL: 'https://malayabits.cc',
+        primaryDomain: 'malayabits',
     },
 
     {
@@ -1452,6 +1463,12 @@ if ( primaryDomain == 'animebytes' ) {
     // Bookmarks | Browse | Details | Playlists
 
     unit3dTrackerHandler('a[href^="https://luminarr.me/torrents/download"]')
+
+} else if ( primaryDomain == 'malayabits' ) {
+    // ----------------------------------- MalayaBits -----------------------------------
+    // Bookmarks | Browse | Details | Playlists
+
+    unit3dTrackerHandler('a[href^="https://malayabits.cc/torrents/download"]')
 
 } else if ( primaryDomain == 'materialize' ) {
     // ----------------------------------- Materialize -----------------------------------
